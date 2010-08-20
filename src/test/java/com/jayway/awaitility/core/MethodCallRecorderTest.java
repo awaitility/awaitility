257412dd0fadc40b0b5f86cb22d2a826923d68ce
Johan Haleby <johan.haleby@gmail.com>
2010-08-20 12:17:59 +0800

    Added license header

--- src/test/java/com/jayway/awaitility/core/MethodCallRecorderTest.java
+++ src/test/java/com/jayway/awaitility/core/MethodCallRecorderTest.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jayway.awaitility.core;
 
+import static com.jayway.awaitility.Awaitility.callTo;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.jayway.awaitility.Awaitility;
 import com.jayway.awaitility.classes.FakeRepository;
 import com.jayway.awaitility.classes.FakeRepositoryImpl;
 
@@ -17,11 +32,11 @@ public class MethodCallRecorderTest {
 	}
 
 	private void recordCall(FakeRepository service) {
-		Awaitility.callTo(service).getValue();
+		callTo(service).getValue();
 	}
 
 	private void suggestGarbageCollection() throws InterruptedException {
-		for (int indx=0; indx<100; indx++) {
+		for (int indx = 0; indx < 100; indx++) {
 			System.gc();
 			Thread.sleep(2);
 		}
