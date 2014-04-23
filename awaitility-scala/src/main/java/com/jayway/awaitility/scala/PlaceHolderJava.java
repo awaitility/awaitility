0cd39b147d006ecebc8644d79c56a2c335c95eb0
Johan Haleby <johan.haleby@gmail.com>
2014-04-23 10:16:16 +0200

    Trying to fix javadoc issues with Sonatype release process

---
+++ awaitility-scala/src/main/java/com/jayway/awaitility/scala/PlaceHolderJava.java
@@ -0,0 +1,8 @@
+package com.jayway.awaitility.scala;
+
+/**
+ * Dummy class used to force Maven to generate Javadocs for the awaitility-scala project. If not then Sonatype will complain that no Javadoc
+ * is present.
+ */
+class PlaceHolderJava {
+}
