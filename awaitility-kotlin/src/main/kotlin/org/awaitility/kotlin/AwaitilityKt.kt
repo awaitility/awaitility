061cca73aee9198db9914ca2f8efe1de555e85d0
Johan Haleby <johan.haleby@gmail.com>
2018-07-05 15:38:19 +0200

    Added several new Kotlin extension methods

--- awaitility-kotlin/src/main/kotlin/org/awaitility/kotlin/AwaitilityKt.kt
+++ awaitility-kotlin/src/main/kotlin/org/awaitility/kotlin/AwaitilityKt.kt
@@ -5,7 +5,29 @@
  */
 package org.awaitility.kotlin
 
+import org.awaitility.Awaitility
+import org.awaitility.Duration
 import org.awaitility.core.ConditionFactory
+import org.awaitility.pollinterval.PollInterval
+import org.hamcrest.Matcher
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.reflect.KClass
+
+/**
+ * This is typically the starting point of the Kotlin "DSL". Allows you to write `await` instead of `await()`. For example:
+ *
+ * ```
+ * await until { myRepository.count() == 2 }
+ * ```
+ * @since 3.1.2
+ */
+val await: ConditionFactory
+    /**
+     * @return A new [ConditionFactory] instance
+     * @see [Awaitility.await]
+     */
+    get() = Awaitility.await()
 
 /**
  * An intermediary data type that stores the [ConditionFactory] and [fn] for later use.
@@ -16,7 +38,7 @@ import org.awaitility.core.ConditionFactory
  * @param fn The function to call in order to extract the value used by the predicate
  * @see untilCallTo
  */
-data class AwaitilityKtUntilFunCondition<T> internal constructor(val factory: ConditionFactory, val fn: () -> T?)
+data class AwaitilityKtUntilFunCondition<T> internal constructor(internal val factory: ConditionFactory, internal val fn: () -> T?)
 
 /**
  * Infix function which is what allows us to write the predicate on right-hand side of [matches] without using a dot.
@@ -29,13 +51,192 @@ infix fun <T> AwaitilityKtUntilFunCondition<T>.matches(pred: (T?) -> Boolean) = 
  * An extension function to `ConditionFactory` that allows you do write conditions such as:
  *
  * ```
- * await().untilCallTo { myRepository.count() } matches { count -> count == 1 }
+ * await untilCallTo { myRepository.count() } matches { count -> count == 1 }
  * ```
  *
- * Note that the reason why we can't name this function [untilCallTo] is because it clashes with
- * `org.awaitility.core.ConditionFactory#until(java.util.concurrent.Callable<java.lang.Boolean>)`.
+ * Note that [await] is a getter that needs to imported from `org.awaitility.kotlin`.
  *
  * @param fn A function that returns the value that will be evaluated by the predicate in [matches].
  * @since 3.1.1
  */
-fun <T> ConditionFactory.untilCallTo(fn: () -> T?) = AwaitilityKtUntilFunCondition(this, fn)
+infix fun <T> ConditionFactory.untilCallTo(fn: () -> T?) = AwaitilityKtUntilFunCondition(this, fn)
+
+/**
+ * An extension function to `ConditionFactory` that allows you do write conditions such as:
+ *
+ * ```
+ * await until { myRepository.count() == 1 }
+ * ```
+ *
+ * Note that [await] is a getter that needs to imported from `org.awaitility.kotlin`.
+ *
+ * @param fn The function to evaluate
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.until(fn: () -> Boolean) = until(fn)
+
+/**
+ * An extension function to `ConditionFactory` that allows you do write conditions such as:
+ *
+ * ```
+ * await withPollInterval ONE_HUNDRED_MILLISECONDS ignoreException IllegalArgumentException::class untilAsserted  {
+ *     assertThat(fakeRepository.value).isEqualTo(1)
+ *}
+ * ```
+ *
+ * I.e. you can use [untilAsserted] to integrate Awaitility with an assertion library of choice.
+ * Note that [await] is a getter that needs to imported from `org.awaitility.kotlin`
+ *
+ * @param fn A function that returns the value that will be evaluated by the predicate in [matches].
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.untilAsserted(fn: () -> Unit): Unit = untilAsserted(fn)
+
+/**
+ * Await at most `timeout` before throwing a timeout exception.
+ *
+ * @param duration the duration
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.atMost(duration: Duration): ConditionFactory = atMost(duration)
+
+/**
+ * Condition has to be evaluated not earlier than `timeout` before throwing a timeout exception.
+ *
+ * @param timeout the timeout
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.atLeast(timeout: Duration): ConditionFactory = atLeast(timeout)
+
+/**
+ * Start building a named await statement. This is useful is cases when you
+ * have several awaits in your test and you need to tell them apart. If a
+ * named await timeout's the <code>alias</code> will be displayed indicating
+ * which await statement that failed.
+ *
+ * @param alias the alias that will be shown if the await timeouts.
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.withAlias(alias: String): ConditionFactory = alias(alias)
+
+/**
+ * Specify the delay that will be used before Awaitility starts polling for
+ * the result the first time. If you don't specify a poll delay explicitly
+ * it'll be the same as the poll interval.
+ *
+ * @param pollDelay the poll delay
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.withPollDelay(pollDelay: Duration): ConditionFactory = pollDelay(pollDelay)
+
+/**
+ * Specify the polling interval Awaitility will use for this await
+ * statement. This means the frequency in which the condition is checked for
+ * completion.
+ *
+ * @param pollInterval the poll interval
+ * @return the condition factory
+ * @since 3.1.2
+ * @see [ConditionFactory.pollInterval]
+ */
+infix fun ConditionFactory.withPollInterval(pollInterval: Duration): ConditionFactory = pollInterval(pollInterval)
+
+/**
+ * Specify the polling interval Awaitility will use for this await
+ * statement. For example [org.awaitility.pollinterval.FibonacciPollInterval.fibonacci].
+ *
+ * @param pollInterval the poll interval
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.withPollInterval(pollInterval: PollInterval): ConditionFactory = pollInterval(pollInterval)
+
+/**
+ * Instruct Awaitility to ignore exceptions instance of the supplied exceptionType type.
+ * Exceptions will be treated as evaluating to <code>false</code>.
+ * This is useful in situations where the evaluated conditions may temporarily throw exceptions.
+ *
+ * @param exceptionType The exception type (hierarchy) to ignore
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.ignoreExceptionsInstanceOf(exceptionType: KClass<out Throwable>): ConditionFactory = ignoreExceptionsInstanceOf(exceptionType.javaObjectType)
+
+/**
+ * Instruct Awaitility to ignore a specific exception and <i>no</i> subclasses of this exception.
+ * Exceptions will be treated as evaluating to <code>false</code>.
+ * This is useful in situations where the evaluated conditions may temporarily throw exceptions.
+ *
+ * @param exceptionType The exception type to ignore
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.ignoreException(exceptionType: KClass<out Throwable>): ConditionFactory = ignoreException(exceptionType.javaObjectType)
+
+/**
+ * Instruct Awaitility to ignore exceptions that occur during evaluation and matches the supplied Hamcrest matcher.
+ * Exceptions will be treated as evaluating to `false`. This is useful in situations where the evaluated
+ * conditions may temporarily throw exceptions.
+ *
+ * @param matcher The Hamcrest matcher
+ * @return the condition factory.
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.ignoreExceptionsMatching(matcher: Matcher<in Throwable>): ConditionFactory = ignoreExceptionsMatching(matcher)
+
+/**
+ * Instruct Awaitility to ignore exceptions that occur during evaluation and matches the supplied Hamcrest matcher.
+ * Exceptions will be treated as evaluating to `false`. This is useful in situations where the evaluated
+ * conditions may temporarily throw exceptions.
+ *
+ * @param matcher The predicate
+ * @return the condition factory.
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.ignoreExceptionsMatching(matcher: (Throwable) -> Boolean): ConditionFactory = ignoreExceptionsMatching(matcher)
+
+/**
+ * Specify the executor service whose threads will be used to evaluate the poll condition in Awaitility.
+ * Note that the executor service must be shutdown manually!
+ *
+ * This is an advanced feature and it should only be used sparingly.
+ *
+ * @param executorService The executor service that Awaitility will use when polling condition evaluations
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.pollExecutorService(executorService: ExecutorService): ConditionFactory = pollExecutorService(executorService)
+
+/**
+ * Specify a thread supplier whose thread will be used to evaluate the poll condition in Awaitility.
+ * The supplier will be called only once and the thread it returns will be reused during all condition evaluations.
+ * This is an advanced feature and it should only be used sparingly.
+ *
+ * @param threadSupplier A supplier of the thread that Awaitility will use when polling
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.pollThread(threadSupplier: (Runnable) -> Thread): ConditionFactory = pollThread(threadSupplier)
+
+/**
+ * Await until a Atomic boolean becomes true.
+ *
+ * @param atomicBoolean the atomic variable
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.untilTrue(atomicBoolean: AtomicBoolean) = untilTrue(atomicBoolean)
+
+/**
+ * Await until a Atomic boolean becomes false.
+ *
+ * @param atomicBoolean the atomic variable
+ * @return the condition factory
+ * @since 3.1.2
+ */
+infix fun ConditionFactory.untilFalse(atomicBoolean: AtomicBoolean) = untilFalse(atomicBoolean)
--- awaitility-kotlin/src/test/kotlin/org/awaitility/kotlin/KotlinTest.kt
+++ awaitility-kotlin/src/test/kotlin/org/awaitility/kotlin/KotlinTest.kt
@@ -16,19 +16,21 @@
 
 package org.awaitility.kotlin
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.catchThrowable
 import org.awaitility.Awaitility.await
+import org.awaitility.Duration.*
 import org.awaitility.classes.Asynch
 import org.awaitility.classes.FakeRepository
 import org.awaitility.classes.FakeRepositoryImpl
 import org.awaitility.core.ConditionTimeoutException
+import org.awaitility.pollinterval.FibonacciPollInterval.fibonacci
 import org.hamcrest.Matchers.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
+import java.util.concurrent.TimeUnit.MILLISECONDS
 import java.util.concurrent.TimeUnit.SECONDS
 import kotlin.test.assertEquals
 
@@ -85,11 +87,38 @@ class KotlinTest {
     }
 
     @Test
+    fun simpleAwaitUntilWithKotlin() {
+        Asynch(fakeRepository).perform()
+
+        await until { fakeRepository.value == 1 }
+    }
+
+    @Test
+    fun usingLotsOfMethodsInDsl() {
+        Asynch(fakeRepository).perform()
+
+        await withAlias "Kotlin Test" ignoreExceptionsInstanceOf
+                IllegalArgumentException::class withPollDelay ONE_HUNDRED_MILLISECONDS withPollInterval
+                fibonacci().with().offset(1).and().timeUnit(MILLISECONDS) atLeast TWO_HUNDRED_MILLISECONDS atMost
+                ONE_MINUTE untilCallTo { fakeRepository.value } matches { it == 1 }
+
+    }
+
+    @Test
+    fun untilAsserted() {
+        Asynch(fakeRepository).perform()
+
+        await withPollInterval ONE_HUNDRED_MILLISECONDS ignoreException IllegalArgumentException::class untilAsserted  {
+            assertThat(fakeRepository.value).isEqualTo(1)
+        }
+    }
+
+    @Test
     fun untilCallToExtensionFnHasADecentErrorMessage() {
         Asynch(fakeRepository).perform()
 
         val throwable = catchThrowable {
-            await().atMost(1, SECONDS).untilCallTo { fakeRepository.value } matches { it == 2 }
+            await() atMost (ONE_SECOND) untilCallTo { fakeRepository.value } matches { it == 2 }
         }
 
         assertThat(throwable).isExactlyInstanceOf(ConditionTimeoutException::class.java).hasMessageEndingWith("expected the predicate to return <true> but it returned <false> for input of <1> within 1 seconds.")
