582652445cd6aece41e8e50113ca3113063990b9
Johan Haleby <johan.haleby@gmail.com>
2019-07-15 10:36:27 +0200

    Making release proses less error prone

--- mvn_release.sh
+++ mvn_release.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to release Awaitility $releaseVersion"
-
+mvn versions:set -DnewVersion=${releaseVersion} && mvn clean install -Prelease -DskipTests=true && \
+git reset HEAD --hard && git clean -df && \
 mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag=awaitility-${releaseVersion} -DreleaseVersion=${releaseVersion} && \
 mvn release:perform -Prelease
 
