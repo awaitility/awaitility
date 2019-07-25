c00c1026f1d7275bb418000bf71b3caaa4c03498
Johan Haleby <johan.haleby@gmail.com>
2019-07-25 09:03:56 +0200

    Added autoboxing test

---
+++ awaitility/src/test/java/org/awaitility/BoxingTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.awaitility;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
+
+@SuppressWarnings("UnnecessaryBoxing")
+public class BoxingTest {
+
+    @Test
+    public void non_primitive_numbers_are_successfully_matched_against_their_primitive_counter_parts() {
+        AtomicInteger ai = new AtomicInteger(0);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            ai.set(2);
+        }).start();
+
+        await().until(() -> new Integer(ai.get()), equalTo(2));
+    }
+}
