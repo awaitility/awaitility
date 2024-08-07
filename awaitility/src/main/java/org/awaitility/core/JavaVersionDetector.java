a4a0a8ba6e7004681b9300fe8c2151ee69370b97
Oleg Estekhin <oleg.estekhin@live.ru>
2024-08-07 11:10:48 +0300

    Support JDK EA builds in JavaVersionDetector (#279)
    * make JavaVersionDetector testable
    
    * add JavaVersionDetectorTest
    
    * detect java major version for jdk-ea builds
    
    * add test for EA with different suffix
    
    * handle any pre-release identifiers
    
    ---------
    
    Co-authored-by: Oleg Estekhin <estekhin@yandex-team.ru>

--- awaitility/src/main/java/org/awaitility/core/JavaVersionDetector.java
+++ awaitility/src/main/java/org/awaitility/core/JavaVersionDetector.java
@@ -4,19 +4,22 @@ public final class JavaVersionDetector {
     private static final String JAVA_VERSION = System.getProperty("java.version");
 
     public static int getJavaMajorVersion() {
+        return getJavaMajorVersion(JAVA_VERSION);
+    }
+
+    static int getJavaMajorVersion(String javaVersion) {
         final String normalizedJavaVersion;
-        if (JAVA_VERSION == null || JAVA_VERSION.isEmpty()) {
+        if (javaVersion == null || javaVersion.isEmpty()) {
             // Fallback to java 8
             normalizedJavaVersion = "8";
-        } else if (JAVA_VERSION.startsWith("1.")) {
-            normalizedJavaVersion = JAVA_VERSION.substring(2, 3);
+        } else if (javaVersion.startsWith("1.")) {
+            normalizedJavaVersion = javaVersion.substring(2, 3);
+        } else if (javaVersion.indexOf('.') != -1) {
+            normalizedJavaVersion = javaVersion.substring(0, javaVersion.indexOf('.'));
+        } else if (javaVersion.indexOf('-') != -1) {
+            normalizedJavaVersion = javaVersion.substring(0, javaVersion.indexOf('-'));
         } else {
-            int dot = JAVA_VERSION.indexOf(".");
-            if (dot != -1) {
-                normalizedJavaVersion = JAVA_VERSION.substring(0, dot);
-            } else {
-                normalizedJavaVersion = JAVA_VERSION;
-            }
+            normalizedJavaVersion = javaVersion;
         }
         return Integer.parseInt(normalizedJavaVersion);
     }
